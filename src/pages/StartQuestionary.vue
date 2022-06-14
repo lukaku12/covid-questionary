89f0ee2a05b653ba6f95a0d7cfb1b390aa9fe949
luka <lukakurdadze2@gmail.com>
2022-06-14 15:10:38 +0400

    feat: setup router && add startQuestionary page

---
+++ src/pages/StartQuestionary.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-center items-center flex-col h-screen">
+    <img src="../assets/images/logo.svg" alt="logo" />
+    <span class="text-3xl font-bold mt-25 text text-center">
+      <router-link :to="{ name: 'question1' }"> კითხვარის დაწყება </router-link>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StartQuestionary",
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+import StartQuestionary from "../pages/StartQuestionary.vue";
+import PersonalInformation from "../pages/PersonalInformation.vue";
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: StartQuestionary, name: "start-question" },
+    {
+      path: "/questionary/1",
+      component: PersonalInformation,
+      name: "question1",
+    },
+    { path: "/:notFound(.*)", redirect: "/" },
+  ],
 });
 
 export default router;
