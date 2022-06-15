2665d356fd37f649b9f6913632ac04aba3798afb
luka <lukakurdadze2@gmail.com>
2022-06-15 15:12:04 +0400

    feat: add error handling && redirect to thank-you page if request is valid

--- src/components/layouts/SubmitFormButton.vue
+++ src/components/layouts/SubmitFormButton.vue
@@ -1,4 +1,5 @@
 <template>
+  <p class="text-right mr-2 text-red-600 mb-3.5">{{ apiError }}</p>
   <button
     type="button"
     class="bg-[#208298] py-3 px-6 rounded-3xl absolute right-2 text-white font-bold"
@@ -13,6 +14,11 @@
 <script>
 export default {
   name: "SubmitFormButton",
+  data () {
+    return {
+      apiError: ''
+    }
+  },
   computed: {
     covidPoliticIsValidValue() {
       return this.$store.getters.covidPoliticIsValid;
@@ -33,22 +39,21 @@ export default {
             antibodies: {
               covid_date: state.CovidState.antibodies.covid_date,
               number: parseInt(state.CovidState.antibodies.number)
-            },
+            }
           }),
-          ...(state.CovidState.had_antibody_test === "no" && {covid_sickness_date: state.CovidState.covid_date.replace(/-/g, "/")})
+          ...(state.CovidState.had_antibody_test === "no" && { covid_sickness_date: state.CovidState.covid_date.replace(/-/g, "/") })
         }),
 
         had_vaccine: state.AreYouVaccinated.had_vaccine === "yes",
-        ...(state.AreYouVaccinated.had_vaccine === "yes" && {vaccination_stage: state.AreYouVaccinated.vaccination_stage}),
-        ...(state.AreYouVaccinated.had_vaccine === "no" && {i_am_waiting: state.AreYouVaccinated.what_are_you_waiting_for,}),
+        ...(state.AreYouVaccinated.had_vaccine === "yes" && { vaccination_stage: state.AreYouVaccinated.vaccination_stage }),
+        ...(state.AreYouVaccinated.had_vaccine === "no" && { i_am_waiting: state.AreYouVaccinated.what_are_you_waiting_for }),
 
         non_formal_meetings: state.CovidPolitic.non_formal_meetings,
         number_of_days_from_office: parseInt(state.CovidPolitic.number_of_days_from_office),
 
-        ...(state.CovidPolitic.what_about_meetings_in_live !== "" && {what_about_meetings_in_live: state.CovidPolitic.what_about_meetings_in_live}),
-        ...(state.CovidPolitic.tell_us_your_opinion_about_us !== "" && {tell_us_your_opinion_about_us: state.CovidPolitic.tell_us_your_opinion_about_us})
+        ...(state.CovidPolitic.what_about_meetings_in_live !== "" && { what_about_meetings_in_live: state.CovidPolitic.what_about_meetings_in_live }),
+        ...(state.CovidPolitic.tell_us_your_opinion_about_us !== "" && { tell_us_your_opinion_about_us: state.CovidPolitic.tell_us_your_opinion_about_us })
       };
-      console.log(sendData);
 
       async function postData(url = "", data = {}) {
         return await fetch(url, {
@@ -60,9 +65,14 @@ export default {
         });
       }
 
+      const router = this.$router;
       postData("https://covid19.devtest.ge/api/create", sendData)
         .then(data => {
-          console.log(data);
+          if (!data.ok) {
+            this.apiError  = 'Something Went Wrong Please Try Again Later!';
+            return;
+          }
+          router.push({ name: "thank-you" });
         });
     }
   }
