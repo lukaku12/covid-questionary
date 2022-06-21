29d9d4126cf3fc1e0d79382323dc3198a30d4185
luka <lukakurdadze2@gmail.com>
2022-06-21 21:44:07 +0400

    feat: add custom error  massages

--- src/config/vee-validate/messages.js
+++ src/config/vee-validate/messages.js
@@ -1,11 +1,13 @@
 import { configure } from "vee-validate";
 import { localize } from "@vee-validate/i18n";
-import en from "@vee-validate/i18n/dist/locale/en.json ";
-import ka from "@vee-validate/i18n/dist/locale/ka.json ";
 
 configure({
-  generateMessage: localize({
-    en,
-    ka,
+  generateMessage: localize('ka', {
+    messages: {
+      required: '{field} ველი აუცილებელია',
+      email: 'თქვენ მიერ შეყვანილი მეილი არასწორია',
+      min: '{field} უნდა შედგებოდეს მინიმუმ 3 სიმბოლოსგან',
+      max: '{field} უნდა შედგებოდეს მაქსიმუმ 255 სიმბოლოსგან',
+    },
   }),
 });
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "@/App.vue";
 import router from "@/router/router.js";
 import store from "@/store";
 import "@/config/vee-validate/rules";
+import "@/config/vee-validate/messages";
 import "@/index.css";
 
 const app = createApp(App);
